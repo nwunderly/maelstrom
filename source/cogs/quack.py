7e127fba001cc6f96c917c2a4bebb329b813110d
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-09-17 00:42:53 -0400

    Create quack.py

---
+++ source/cogs/quack.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+
+B = ('B', '🅱️')
+QUACK = '<a:quack:875209729117413387>'
+
+
+class Quack(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content in B:
+            await message.add_reaction(QUACK)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if reaction.emoji == '🅱️':
+            await reaction.message.reply(QUACK)
+
+
+def setup(bot):
+    bot.add_cog(Quack(bot))
